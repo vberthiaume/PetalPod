f4137c3792def2910abceced9e9a9c952c365bc5
Vincent Berthiaume <vberthiaume@gmail.com>
2024-07-02 13:33:48 -0400

    initial commit

---
+++ pod/SimpleOscillator/SimpleOscillator.cpp
@@ -0,0 +1,77 @@
+#include "daisy_pod.h"
+#include "daisysp.h"
+
+#define NUM_WAVEFORMS 4
+
+using namespace daisy;
+using namespace daisysp;
+
+DaisyPod   hw;
+Oscillator osc;
+Parameter  p_freq;
+
+uint8_t waveforms[NUM_WAVEFORMS] =
+{
+    Oscillator::WAVE_SIN,
+    Oscillator::WAVE_TRI,
+    Oscillator::WAVE_POLYBLEP_SAW,
+    Oscillator::WAVE_POLYBLEP_SQUARE,
+};
+
+void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
+                   AudioHandle::InterleavingOutputBuffer out,
+                   size_t                                size)
+{
+    hw.ProcessDigitalControls();
+
+    static int waveform {0};
+    static int octave {0};
+
+    waveform += hw.encoder.Increment();
+    waveform = DSY_CLAMP(waveform, 0, NUM_WAVEFORMS);
+    osc.SetWaveform(waveforms[waveform]);
+
+    if(hw.button2.RisingEdge())
+        octave++;
+    if(hw.button1.RisingEdge())
+        octave--;
+    octave = DSY_CLAMP(octave, 2, 4);
+
+    // convert MIDI to frequency and multiply by octave size
+    const auto freq = mtof(p_freq.Process() + (octave * 12));
+    osc.SetFreq(freq);
+
+    // Audio Loop
+    for(size_t i = 0; i < size; i += 2)
+    {
+        // Process
+        const auto sig = osc.Process();
+        out[i] = sig;
+        out[i + 1] = sig;
+    }
+}
+
+void InitSynth(float samplerate)
+{
+    // Init freq Parameter to knob1 using MIDI note numbers
+    // min 10, max 127, curve linear
+    p_freq.Init(hw.knob1, 0, 127, Parameter::LINEAR);
+
+    osc.Init(samplerate);
+    osc.SetAmp(1.f);
+}
+
+int main(void)
+{
+    // Init everything
+    hw.Init();
+    hw.SetAudioBlockSize(4);
+    const auto samplerate = hw.AudioSampleRate();
+    InitSynth(samplerate);
+
+    // start callbacks
+    hw.StartAdc();
+    hw.StartAudio(AudioCallback);
+
+    while(1) {}
+}
