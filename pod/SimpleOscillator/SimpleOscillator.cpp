b6b6c72e99da7d45d50a80fc75afca2f168c7c6e
Vincent Berthiaume <vberthiaume@gmail.com>
2024-07-02 18:17:01 -0400

    using second pot as volume

--- pod/SimpleOscillator/SimpleOscillator.cpp
+++ pod/SimpleOscillator/SimpleOscillator.cpp
@@ -8,7 +8,8 @@ using namespace daisysp;
 
 DaisyPod   hw;
 Oscillator osc;
-Parameter  p_freq;
+Parameter  frequencyParameter;
+Parameter  volumeParameter;
 
 uint8_t waveforms[NUM_WAVEFORMS] =
 {
@@ -25,24 +26,25 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
     hw.ProcessDigitalControls();
 
     static int waveform {0};
-    static int octave {0};
-
     waveform += hw.encoder.Increment();
     waveform = DSY_CLAMP(waveform, 0, NUM_WAVEFORMS);
     osc.SetWaveform(waveforms[waveform]);
 
-    if(hw.button2.RisingEdge())
-        octave++;
-    if(hw.button1.RisingEdge())
-        octave--;
+    static int octave {0};
+    if (hw.button2.RisingEdge())
+        ++octave;
+    if (hw.button1.RisingEdge())
+        --octave;
     octave = DSY_CLAMP(octave, 0, 4);
 
     // convert MIDI to frequency and multiply by octave size
-    const auto freq = mtof(p_freq.Process() + (octave * 12));
-    osc.SetFreq(freq);
+    const auto freq = mtof (frequencyParameter.Process() + (octave * 12));
+    osc.SetFreq (freq);
+
+    osc.SetAmp (volumeParameter.Process());
 
     // Audio Loop
-    for(size_t i = 0; i < size; i += 2)
+    for (size_t i = 0; i < size; i += 2)
     {
         // Process
         const auto sig = osc.Process();
@@ -51,23 +53,18 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
     }
 }
 
-void InitSynth(float samplerate)
-{
-    // Init freq Parameter to knob1 using MIDI note numbers
-    // min 10, max 127, curve linear
-    p_freq.Init(hw.knob1, 0, 127, Parameter::LINEAR);
-
-    osc.Init(samplerate);
-    osc.SetAmp(1.f);
-}
-
 int main(void)
 {
     // Init everything
     hw.Init();
     hw.SetAudioBlockSize(4);
-    const auto samplerate = hw.AudioSampleRate();
-    InitSynth(samplerate);
+
+    // Init freq Parameter to knob1 using MIDI note numbers min 0, max 127, curve linear
+    frequencyParameter.Init (hw.knob1, 0, 127, Parameter::LINEAR);
+    volumeParameter.Init (hw.knob2, 0.f, 1.f, Parameter::LINEAR);
+
+    osc.Init(hw.AudioSampleRate());
+    // osc.SetAmp (1.f);
 
     // start callbacks
     hw.StartAdc();
